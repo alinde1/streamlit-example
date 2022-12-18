2730ced5d7b5c8ed569c40a9d0164652413d7946
Antonio Linde <alinde@hotelbeds.com>
2022-12-18 18:41:12 +0100

    Update main app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,24 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+import time
 
-st.title("Uber pickups in NYC")
+st.set_page_config(layout="wide")
+st.title('Logging in Text Box')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+# creating a placeholder for the fixed sized textbox
+logtxtbox = st.empty()
+logtxt = 'start'
+logtxtbox.text_area("Logging: ",logtxt, height = 500)
 
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+end_of_loop = False
+counter = 1
 
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! (using st.cache)")
+while (end_of_loop==False):
 
-st.subheader('Raw data')
-st.write(data)
+    logtxt += 'Counter [' + str(counter) + '] \n'
+    logtxtbox.text_area("Logging: ", logtxt, height=500)
 
-message = st.text("Prueba")
+    counter += 1
+    if (counter > 100):
+        end_of_loop = True
 
-import string
-import random
-
-def update_text():
-    text = ''.join(random.choices(string.ascii_uppercase + string.digits, k=7))
-    message.text(text)
-
-st.text_input("Introduce algo", value="", on_change=update_text)
+    time.sleep(0.2)
