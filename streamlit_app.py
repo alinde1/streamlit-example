0f1f82233b279bcd1dcf40baac28431a355127b1
Antonio Linde <alinde@hotelbeds.com>
2023-01-22 20:00:54 +0100

    Remove pickle.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@
 import streamlit as st
 from streamlit_chat import message
 import requests
+import simpletransformers
+import pickle
 
 st.set_page_config(
     page_title="Chatbot para refugiados",
@@ -21,6 +23,30 @@ if 'past' not in st.session_state:
     st.session_state['past'] = []
 
 
+def load_model():
+    model_path = 'https://github.com/josuemzx/Chatbot-para-refugiados/blob/main/models/model.pkl?raw=true'
+    r = requests.get(model_path, stream='True')
+    model = pickle.load(r.raw)
+    return model
+
+
+def prediction(context, question, limit=5):
+  to_predict = [
+      {
+          "context": context,
+          "qas": [
+              {
+                  "question": question,
+                  "id": "0",
+              }
+          ],
+      }
+  ]
+  answers, probabilities = model.predict(to_predict)
+  answers = answers[0]["answer"][:limit]
+  return answers
+
+
 def query(payload):
     st.write(payload)
     response = requests.post(API_URL, headers=headers, json=payload)
@@ -32,6 +58,21 @@ def get_text():
     return input_text
 
 
+model = load_model()
+contexto_1 = """
+Derecho a solicitar protección internacional.\n\n1. Las personas nacionales no comunitarias
+y las apátridas presentes en territorio español tienen derecho a solicitar protección internacional en España.\n\n2.
+Para su ejercicio, los solicitantes de protección internacional tendrán derecho a asistencia sanitaria y a asistencia
+jurídica gratuita, que se extenderá a la formalización de la solicitud y a toda la tramitación del procedimiento,
+y que se prestará en los términos previstos en la legislación española en esta materia, así como derecho a intérprete
+en los términos del artículo 22 de la Ley Orgánica 4/2000.\n\nLa asistencia jurídica referida en el párrafo anterior
+será preceptiva cuando las solicitudes se formalicen de acuerdo al procedimiento señalado en el artículo 21 de la
+presente Ley.\n\n3. La presentación de la solicitud conllevará la valoración de las circunstancias determinantes
+del reconocimiento de la condición de refugiado, así como de la concesión de la protección subsidiaria.
+De este extremo se informará en debida forma al solicitante.\n\n4. Toda información relativa al procedimiento,
+incluido el hecho de la presentación de la solicitud, tendrá carácter confidencial.
+"""
+
 user_input = get_text()
 
 if user_input:
@@ -42,7 +83,7 @@ if user_input:
     #         "text": user_input,
     #     },"parameters": {"repetition_penalty": 1.33},
     # })
-    output = "Probando..."
+    output = prediction(contexto_1, user_input)[-1]
 
     # st.write(user_input)
     # st.write(output)
